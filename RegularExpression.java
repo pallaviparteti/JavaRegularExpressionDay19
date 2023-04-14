934763eee765b4c5d9c36bb56db617025f201228
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-04-15 00:13:33 +0530

    Added: file for the validLirstName

--- RegularExpression.java
+++ RegularExpression.java
@@ -5,26 +5,25 @@ import java.util.regex.Pattern;
 
 
 public class RegularExpression {
-		public static void main(String[] args) {
-		    String regex = "^[A-Z][a-zA-Z]{2,}$";
+	public static void main(String[] args) {
+		String regex = "^[A-Z][a-zA-Z]{2,}$";
 
-		    Scanner sc = new Scanner(System.in);
-		    System.out.println("Enter The First Name : ");
-			String data = sc.nextLine();
-	        
-			
-			Pattern pattern = Pattern.compile(regex);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The First Name : ");
+		String firstName = sc.nextLine();
+		System.out.println("Enter The Last Name : ");
+		String lastName = sc.nextLine();
 
-			Matcher matcher = pattern.matcher(data);
-			boolean result = matcher.matches();
+		Pattern pattern = Pattern.compile(regex);
 
-		    if (result) {
-		        System.out.println("Valid first name");
-		    } else {
-		        System.out.println("Pattern not matched");
-		    }
+		Matcher matcher = pattern.matcher(firstName + lastName);
+		boolean result = matcher.matches();
+
+		if (result) {
+			System.out.println("Valid first name and last name");
+		} else {
+			System.out.println("Pattern not matched");
+		}
 
 	}
-	
-
 }
